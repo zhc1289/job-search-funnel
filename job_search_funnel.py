3e6c8b9293cc627e11a854944b53433504cbd375
zhi <zhc1289@gmail.com>
2019-07-16 16:54:14 -0400

    testing first push

---
+++ job_search_funnel.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# to activate environment
+# python -m venv env
+# source ./env/bin/activate
+
+url = "https://www.indeed.com/jobs?q=blockchain+$140,000&l=New+York+State&explvl=entry_level"
+
+indeedFile = urlopen(url)
+indeedHtml = indeedFile.read()
+indeedFile.close()
+
+# added parameter html parser to get rid of a parsing warning
+soup = BeautifulSoup(indeedHtml, "html.parser")
+
+for item in soup.find_all('div', attrs={'class':'title'}):
+    print(soup.find('a').get('href') )

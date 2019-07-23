66e10672d3844e9dca10fa5732f7e1e569dfe059
zhi <zhc1289@gmail.com>
2019-07-23 18:13:31 -0400

    application now opens up to 30 search results in 30 new tabs

--- job_search_funnel.py
+++ job_search_funnel.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from webbrowser import open_new_tab
+from shutil import copyfileobj
 
 # to activate environment
 # python -m venv env
@@ -14,9 +16,16 @@ indeedFile.close()
 # added parameter html parser to get rid of a parsing warning
 soup = BeautifulSoup(indeedHtml, "html.parser")
 
-for item in soup.find_all('div', attrs={'class':'title'}):
-    domain = 'https://www.indeed.com'
-    link = item.find("a").get("href")
-    link = domain + link
-    print(item.text)
-    print(link)
+count = 0
+while count < 30:
+    for item in soup.find_all('div', attrs={'class':'title'}):
+        domain = 'https://www.indeed.com'
+        link = item.find("a").get("href")
+        page = "&start="+ str(count)
+        link = domain + link + page
+        with urlopen(link) as response, open('/home/zhi/Desktop/job_search_funnel/tester.txt', 'wb') as out_file:
+            copyfileobj(response, out_file)
+            open_new_tab(link)
+        
+        print(item.text)
+    count += 10
---
+++ reader.py
@@ -0,0 +1,3 @@
+if 'Python' in open('/home/zhi/Desktop/job_search_funnel/tester.txt').read():
+    print("true")
+else: print("false")

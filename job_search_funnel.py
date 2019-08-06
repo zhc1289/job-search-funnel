73371e20ea74f006c2235c0a7ccb6224351a258f
zhi <zhc1289@gmail.com>
2019-08-06 12:49:13 -0400

    added tab closing and filtered out jobs which require me to apply at the company's site instead of indeed

--- job_search_funnel.py
+++ job_search_funnel.py
@@ -2,11 +2,16 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from webbrowser import open_new_tab
 from shutil import copyfileobj
-import csv
+from pyautogui import typewrite, time, hotkey, PAUSE, FAILSAFE
+import csv, time
 
 # to activate environment
 # python -m venv env
 # source ./env/bin/activate
+
+# need to install bs4 and pyautogui
+# pyautogui failsafe moving mouse to upperleft of screen will stop the program
+FAILSAFE = True
 
 url = "https://www.indeed.com/jobs?q=blockchain%2B&l=New%2BYork%2BState&explvl=entry_level"
 
@@ -23,13 +28,29 @@ soup = BeautifulSoup(indeedHtml, "html.parser")
 
 count = 0
 jobNumber = 0
-while count < 70:
+while count < 19:
 
     for item in soup.find_all('div', attrs={'class':'title'}):
         
         # removes all sponsored jobs
         if item.parent.find('span', attrs={'class':'sponsoredGray'}):
-            print(item)
+            item.decompose()
+
+        # removes all jobs that have me apply through company site instead of Indeed resume
+        elif not item.parent.find('span', attrs={'class':'iaLabel'}):
+            domain = 'https://www.indeed.com'
+            link = item.find("a").get("href")
+            page = "&start=" + str(count)
+            link = domain + link + page
+
+            # opens a new tab for each apply through company link
+            with urlopen(link) as response, open('/home/zhi/Desktop/job_search_funnel/tester.txt', 'wb') as out_file:
+                copyfileobj(response, out_file)
+                open_new_tab(link)
+
+            # writes to csv file
+            f.writerow([item.text, link])
+            jobNumber += 1   
             item.decompose()
         
         else:
@@ -38,13 +59,12 @@ while count < 70:
             page = "&start=" + str(count)
             link = domain + link + page
 
-            
-            # opens a new tab for each link
-            # with urlopen(link) as response, open('/home/zhi/Desktop/job_search_funnel/tester.txt', 'wb') as out_file:
-                #copyfileobj(response, out_file)
-                #open_new_tab(link)
+            with urlopen(link) as response, open('/home/zhi/Desktop/job_search_funnel/tester.txt', 'wb') as out_file:
+                copyfileobj(response, out_file)
+                open_new_tab(link)
+                time.sleep(2)
+                hotkey('ctrl', 'w')
 
-            # writes to csv file    
             f.writerow([item.text, link])
             jobNumber += 1       
  
